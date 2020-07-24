784938a6ac9f217e8cf2a8c288ff478668cac7ba
pangchuan99 <1248354637@qq.com>
2020-07-24 11:17:20 +0800

    web自动化添加文件

---
+++ api_pytest_2020/PythonLianXi/TestCases/test_002.py
@@ -0,0 +1,19 @@
+
+import pytest
+from selenium import webdriver
+
+
+def test_002():
+    a=1+1
+    return a
+
+
+def test_003():
+    a = 1 + 3
+    return a
+
+
+
+def test_004():
+    a = 2 + 5
+    return a
